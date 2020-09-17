ed45bffc7665519722ddd70ffcce0f7dd6eed641
Ian Muñoz Nuñez <ian.munoz4644@alumnos.udg.mx>
2020-09-17 18:28:03 -0500

    Se captura la edad y se imprime esta mas uno

--- main.c
+++ main.c
@@ -4,9 +4,12 @@
 int main()
 {
     char nom[40];
+    unsigned int e;
     printf("Que tal?...\n\n");
     printf("Como te llamas?...\n");
     fgets(nom, sizeof(nom), stdin);
-    printf("\n\nHola %s\n\n", nom);
+    printf("Ingresa tu edad: ");
+    scanf("%u", &e);
+    printf("\n\nHola %s, vas a cumplir %u", nom, e+1);
     return 0;
 }
