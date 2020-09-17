c426df6b1c0cd1bead9811afba4da4f9ecb37e4f
Ian Muñoz Nuñez <ian.munoz4644@alumnos.udg.mx>
2020-09-17 17:48:38 -0500

    Se captura el nombre completo con fgets()

--- main.c
+++ main.c
@@ -3,6 +3,10 @@
 
 int main()
 {
-    printf("Hello world!\n");
+    char nom[40];
+    printf("Que tal?...\n\n");
+    printf("Como te llamas?...\n");
+    fgets(nom, sizeof(nom), stdin);
+    printf("\n\nHola %s\n\n", nom);
     return 0;
 }
